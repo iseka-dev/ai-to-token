3a90beae6b14f4c6f99bf3b4d92364a1de0ed434
Dev Iseka <horacio.chiarella@gmail.com>
2023-04-26 01:59:49 -0300

    better routing and wip with layering arch

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,11 +1,14 @@
 import logging
 import sys
 
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 log = logger
--- src/routes/v1/home_routes.py
+++ src/routes/v1/home_routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 from fastapi import Request
+from fastapi import status
 from fastapi.templating import Jinja2Templates
+
+from src.core.logger import log
 
 
 home_routes = APIRouter(
@@ -10,6 +14,14 @@ home_routes = APIRouter(
 
 templates = Jinja2Templates(directory="templates")
 
+
 @home_routes.get("/")
 async def home_html(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    try:
+        return templates.TemplateResponse("home.html", {"request": request})
+    except Exception as e:
+        log.error(f"Route Error: {e}")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail={"error_home": "Home site not available"}
+    )
