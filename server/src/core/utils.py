41d7b38b50b608b4458ae8967b66d12d173200e8
Horacio <hchiarella@spins.com>
2023-06-22 18:12:48 -0300

    unused fastapi_vite package removed

--- server/src/core/utils.py
+++ server/src/core/utils.py
@@ -1,6 +1,3 @@
-import fastapi_vite
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="../client/templates")
-templates.env.globals['vite_hmr_client'] = fastapi_vite.vite_hmr_client
-templates.env.globals['vite_asset'] = fastapi_vite.vite_asset
