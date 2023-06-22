2ac36e05be662a8c05d662299096ff0f24993ad6
Horacio <hchiarella@spins.com>
2023-06-22 14:21:38 -0300

    fixtures placed in conf.test

---
+++ server/src/tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+
+client = TestClient(app)
+
+
+@pytest.fixture()
+def client():
+    client = TestClient(app)
+    yield client
--- server/src/tests/unit/test_health_check_route.py
+++ server/src/tests/unit/test_health_check_route.py
@@ -1,21 +1,3 @@
-import os
-import pytest
-
-from fastapi.testclient import TestClient
-
-from src.main import app
-
-print(os.getcwd())
-
-client = TestClient(app)
-
-
-@pytest.fixture()
-def client():
-    client = TestClient(app)
-    yield client
-
-
 def test_get_health_check_route(client):
     response = client.get("/health-check")
     print(response)
